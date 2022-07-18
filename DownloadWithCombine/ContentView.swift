cf53ad93fe93bd6f4d9b5e73b141f7e918c41115
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-18 12:50:55 +0200

    Initial Commit

---
+++ DownloadWithCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DownloadWithCombine
+//
+//  Created by Skorobogatow, Christian on 18/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DownloadWithCombine/DownloadWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithCombineApp.swift
+//  DownloadWithCombine
+//
+//  Created by Skorobogatow, Christian on 18/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
